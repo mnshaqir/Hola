22544fd494380deee38fa9a34ba0dd78c0cb684b
mnshaqir <116859685+mnshaqir@users.noreply.github.com>
2022-11-22 14:41:56 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Hola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HolaLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HolaLabel
+            // 
+            this.HolaLabel.AutoSize = true;
+            this.HolaLabel.BackColor = System.Drawing.Color.FloralWhite;
+            this.HolaLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HolaLabel.Font = new System.Drawing.Font("Modern No. 20", 47.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HolaLabel.Location = new System.Drawing.Point(186, 106);
+            this.HolaLabel.Name = "HolaLabel";
+            this.HolaLabel.Size = new System.Drawing.Size(378, 65);
+            this.HolaLabel.TabIndex = 0;
+            this.HolaLabel.Text = "Hola Amigos";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(321, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.HolaLabel);
+            this.Name = "Form1";
+            this.Text = "Hola ";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HolaLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (HolaLabel.Text == "Hola Amigos")
+            {
+                HolaLabel.Text = "Hai Semua";
+            }
+            else if (HolaLabel.Text == "Hai Semua")
+            {
+                HolaLabel.Text = "Apa Khabar";
+            }
+            else if (HolaLabel.Text == "Apa Khabar")
+            {
+                HolaLabel.Text = "Dah makan ?";
+            }
+            else 
+            {
+                HolaLabel.Text = "Hola Amigos";
+            }
+        }
+    }
+}
